c8ec904fe83784981fa32cc6c4cf33c32e93377e
Jesse64 <jelle.streekstra@hotmail.com>
2018-03-04 13:50:10 +0100

    Adds basic movement

--- paperds/src/player/NormalBehavior.cpp
+++ paperds/src/player/NormalBehavior.cpp
@@ -17,5 +17,23 @@ NormalBehavior::~NormalBehavior()
 
 void NormalBehavior::Update()
 {
-	
+	u16 pad = PAD_Read();
+	if (pad & (PAD_KEY_RIGHT | PAD_KEY_LEFT | PAD_KEY_UP | PAD_KEY_DOWN))
+	{
+		VecFx32 direction = { 0, 0, 0 };
+
+		if (pad & PAD_KEY_RIGHT)
+			direction.x = FX32_ONE;
+		else if (pad & PAD_KEY_LEFT)
+			direction.x = -FX32_ONE;
+
+		if (pad & PAD_KEY_UP)
+			direction.z = -FX32_ONE;
+		else if (pad & PAD_KEY_DOWN)
+			direction.z = FX32_ONE;
+
+		VEC_Normalize(&direction, &direction);
+		_player->SetDirection(&direction);
+		_player->AddForceOnDirection(FX32_CONST(0.3));
+	}
 }
